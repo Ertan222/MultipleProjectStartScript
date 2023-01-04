667e4e3cedb44cf52fdce4222575aa0271f3104d
Ertan222 <ertantaner2000mail.com>
2023-01-04 16:16:41 +0300

    scripts enchaned

--- startclinic.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-dotnet run --project APIConsumeTry/APIConsumeTry.csproj &
-dotnet run --project CitiesConsumingAPI/CitiesConsumingAPI.csproj &
--- stopclinic.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-killpids=$(pgrep -f "dotnet run")
-kill $killpids
